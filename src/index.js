2b08be2a077b89aa559224e8c732222a253232b8
simonnj <sije817@student.sdu.dk>
2019-10-05 19:35:25 +0300

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -21,30 +21,45 @@ var winningValues = [
   17043521, //right to left diagnoal
   1118480 //left to right diagonal
 ];
+var table = document.getElementById("tictactoeTable");
+var tbody = table.getElementsByTagName("tbody")[0];
+var cells = tbody.getElementsByTagName("td");
 
 var gameOver = false;
 
-var table = "";
+// var table = "";
 
-for (var r = 0; r < sizeOfBoard; r++) {
+/* for (var r = 0; r < sizeOfBoard; r++) {
   table += "<tr>";
   for (var c = 1; c <= sizeOfBoard; c++) {
     table += "<td 'onclick=placeMark(this)'> </td>";
   }
   table += "</tr>";
-}
+} */
 document.getElementById("board").innerHTML = "<table>" + table + "</table>";
 
-document.getElementById("board").addEventListener("click", placeMark);
+document.getElementById("tictactoeTable").addEventListener("click", placeMark);
 
 function placeMark(points) {
   if (gameOver === false)
-    document.getElementById("board").innerHTML = "" + players[turn];
+    document.getElementsByTagName("td").innerHTML = "" + players[turn];
   sizeOfBoard++;
   pointCount(points);
   winning();
+  changeCellColor();
   if (gameOver === false) {
     switchPlayer();
+  }
+}
+function changeCellColor() {
+  for (var i = 0, length = cells.length; i < length; i++) {
+    if ((cells[i].innerHTML = "X")) {
+      (cells[i].style.backgroundColor = 124), 252, 0;
+    } else if ((cells[i].innerHTML = "O")) {
+      (cells[i].style.backgroundColor = 250), 128, 114;
+    } else {
+      cells[i].style.backgroundColor = "white";
+    }
   }
 }
 
